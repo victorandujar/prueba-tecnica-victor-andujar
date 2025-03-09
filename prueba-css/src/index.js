543ef26b0b3c315ac32ecda3f333128296238e72
victorandujar <victorandujarmurcia@gmail.com>
2025-03-09 13:12:00 +0100

    Modify html structure & add its styles

--- prueba-css/src/index.js
+++ prueba-css/src/index.js
@@ -1,26 +1,43 @@
-
 var arrayTest = [1, 2, 3, 3, 2, 1, 4];
 
 document.getElementById("app").innerHTML = `
-  <h1>Unique Element Challenge</h1>
+  <section class="header">
+    <h1 class="title">Unique Element Challenge</h1>
+    <img src="./src/assets/logo.svg" alt="Logo" class="image"/>
+  </section>
+  <div class="container">
+    <section class="instructions_section">
+      <div class="section-content">
+        <h2 class="section-title">Target</h2>
+        <span class="">Find the unique value in the array that is not duplicated. Use the <b>dubbed</b> function to return the unique element.</span>
+      </div>
+      <div class="section-content">
+        <h2 class="section-title">Considerations</h2>
+        <ul class="list">
+          <li>Values are duplicated only twice.</li>
+          <li>There is only one non duplicate value.</li>
+          <li>The non duplicate value can be placed anywhere on the array.</li>
+          <li>The result must be an integer.</li>
+        </ul>
+      </div>
+    </section>
+    <section class="results-section">
+      <div class="test-data-box">
+        <h3 class="results-title">Test data</h3>
+        <span>${arrayTest}</span>
+      </div>
+      <section class="results">
+        <div class="expected-result">
+          <h3 class="results-title">Expected result</h3>
+          <span>4</span>
+        </div>
+        <div class="your-result">
+          <h3 class="results-title">Your result</h3>
+          <span>4</span>
+        </div>
+      </section>
+    </section>
+  </div>
+</div>
 
-  <h2>Target</h2>
-  <div>Find the unique value in the array that is not duplicated. Use the <b>dubbed</b> function to return the unique element.</div>
-
-  <h2>Considerations</h2>
-  <ul>
-    <li>Values are duplicated only twice.</li>
-    <li>There is only one non duplicate value.</li>
-    <li>The non duplicate value can be placed anywhere on the array.</li>
-    <li>The result must be an integer.</li>
-  </ul>
-  
-  <h2>Test Data</h2>
-  <div>${arrayTest}</div>
-    
-  <h2>Expected result</h2>
-  <div>4</div>
-
-  <h2>Your Result</h2>
-  <div>4</div>
 `;
